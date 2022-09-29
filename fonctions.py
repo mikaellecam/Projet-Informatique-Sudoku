1c8428cfdf3623b643d738dcc28c2eb094101e1f
Mikael Le Cam <mikael.california@gmail.com>
2022-09-29 21:45:12 +0200

    avancement sur la dernière fonction

--- fonctions.py
+++ fonctions.py
@@ -16,7 +16,7 @@ def verifier_bloc(G,i):
 def verifier_ligne(G,i):
     l = []
     for x in G[i]:
-        if x>0 and x<=4:
+        if 0<x<=4:
             if x not in l or x==0:
                 l.append(x)
             else:
@@ -26,17 +26,19 @@ def verifier_ligne(G,i):
 def verify_column(G,j):
     l = []
     for i in range(len(G)):
-        if G[i][j] > 0 and G[i][j] <= 4:
+        if 0< G[i][j] <= 4:
             if G[i][j] not in l:
                 l.append(G[i][j])
             else:
                 return False
     return True
 
-def afficherline(G,l):
-    affichage = f"\n| {G[l][0]} | {G[l][1]} || {G[l][2]} | {G[l][3]} |"
-    return affichage
-
+def est_complete(G):
+    for k in range(len(G)):
+        for i in range(len(G)):
+            if G[k][i] == 0:
+                return False
+    return True
 
 def afficherline(G,l):
     affichage = f"\n| {G[l][0]} | {G[l][1]} || {G[l][2]} | {G[l][3]} |"
--- sudoku.py
+++ sudoku.py
@@ -1,8 +1,7 @@
-import random
-from fonctions import verifier_bloc,verifier_ligne,verify_column
+from fonctions import verifier_bloc,verifier_ligne,verify_column,est_complete
 
 #N = int(input("Taille de grille: "))
-grille = [[1,4,3,2],[3,1,1,4],[4,1,2,3],[2,3,4,1]]
+grille = [[1,4,3,2],[3,2,0,4],[4,1,2,3],[2,3,4,1]]
 
 def generationgrille(N):
     pass
@@ -16,7 +15,7 @@ def afficher(G):
         affichage += "\n" + "-"*(len(G)**2 + 2)
     return affichage
 
-print(afficher(grille))
+#print(afficher(grille))
 
 def verification(G):
     for k in range(len(G)):
@@ -26,5 +25,28 @@ def verification(G):
             return False
         if not verify_column(G,k):
             return False
+        if not est_complete(G):
+            return False
     return True
-print(verification(grille))
+#print(verification(grille))
+
+def resolution(G):
+    if len(G) == 0 or not verification(G):
+        return None
+    if verification(G):
+        return afficher(G)
+    for i in range(len(G)):
+        for j in range(len(G)):
+            if G[i][j] == 0:
+                x,y = i,j
+    for k in range(1,len(G)+1):
+        G[x][y] = k
+        res = resolution(G)
+        if res != None:
+            return res
+        
+
+
+
+
+print(resolution(grille))
