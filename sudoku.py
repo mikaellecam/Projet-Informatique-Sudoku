4d68473203e9f827b01138d486615add871884c3
paulpvc <paul.vigeolas@gmail.com>
2022-09-30 23:20:22 +0200

    j'ai réglé le problème de la fonction résoudre, en corrigeant la fonction verification et j'ai ajusté quelques PEP pour que ce soit plus lisible. La fonction résolution marche bien sur l'exemple mais à voir sur plusieurs grilles

--- sudoku.py
+++ sudoku.py
@@ -1,7 +1,8 @@
 from fonctions import verifier_bloc,verifier_ligne,verify_column,est_complete
 
-#N = int(input("Taille de grille: "))
-grille = [[1,4,3,2],[3,2,0,4],[4,1,2,3],[2,3,4,1]]
+# N = int(input("Taille de grille: "))
+grille = [[1,4,3,2], [3,2,0,4], [4,1,2,3], [2,3,4,1]]
+grille_test_aresoudre = [[0,1,0,0], [0,4,0,0], [0,0,0,0], [1,0,0,4]]
 
 def generationgrille(N):
     pass
@@ -10,43 +11,43 @@ def afficher(G):
     affichage = "------------------"
     for k in range(len(G)):
         affichage += f"\n| {G[k][0]} | {G[k][1]} || {G[k][2]} | {G[k][3]} |"
-        if k==1:
+        if k == 1:
             affichage += "\n------------------"
         affichage += "\n" + "-"*(len(G)**2 + 2)
     return affichage
 
-#print(afficher(grille))
+# print(afficher(grille))
 
 def verification(G):
     for k in range(len(G)):
-        if not verifier_ligne(G,k):
+        if not verifier_ligne(G, k):
             return False
-        if not verifier_bloc(G,k):
+        if not verifier_bloc(G, k):
             return False
-        if not verify_column(G,k):
+        if not verify_column(G, k):
             return False
-        if not est_complete(G):
-            return False
+        """ if not est_complete(G): # ce bloc casse la résolution, y aura toujours des 0 avant
+                                   # d'avoir trouvé une solution donc on peut pas le mettre
+            return False"""
     return True
-#print(verification(grille))
+# print(verification(grille))
 
 def resolution(G):
-    if len(G) == 0 or not verification(G):
+    verif = verification(G)
+    if len(G) == 0 or not verif:
         return None
-    if verification(G):
+    if est_complete(G):
         return afficher(G)
     for i in range(len(G)):
         for j in range(len(G)):
             if G[i][j] == 0:
-                x,y = i,j
+                x, y = i, j
     for k in range(1,len(G)+1):
         G[x][y] = k
         res = resolution(G)
         if res != None:
             return res
-        
 
 
-
-
-print(resolution(grille))
+# print(resolution(grille))
+print(resolution(grille_test_aresoudre))
