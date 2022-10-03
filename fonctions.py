216fd218cbf7700b1276c1fc9acbbc71ee2c2093
Mikael Le Cam <mikael.california@gmail.com>
2022-10-03 12:58:12 +0200

    formatting de fichier

--- fonctions.py
+++ fonctions.py
@@ -1,4 +1,15 @@
 from math import sqrt
+import random
+
+def afficher(G):
+    affichage = "------------------"
+    for k in range(len(G)):
+        affichage += f"\n| {G[k][0]} | {G[k][1]} || {G[k][2]} | {G[k][3]} |"
+        if k == 1:
+            affichage += "\n------------------"
+        affichage += "\n" + "-"*(len(G)**2 + 2)
+    return affichage
+
 
 def verifier_bloc(G,i):
     x,y = (i//sqrt(len(G))*sqrt(len(G))),(i%sqrt(len(G))*sqrt(len(G)))
@@ -43,3 +54,27 @@ def est_complete(G):
 def afficherline(G,l):
     affichage = f"\n| {G[l][0]} | {G[l][1]} || {G[l][2]} | {G[l][3]} |"
     return affichage
+
+def generationgrille(N):
+    nb = random.randint(abs((N**2)//3 - 1), (N**2)//3) # on ne rempli qu'a peu près 1/3 de la grille au préalable (difficulté)
+    grid = [[0]*N for _ in range(N)] # ambiguité de python on ne peut pas écrire [[0]*N]*N (a cause de la mutabilitée)
+    i = 0
+    while i < nb:
+        x, y = (random.randint(0, N-1), random.randint(0, N-1))
+        #print(x,y, grid[x][y], i)
+        if grid[x][y] == 0:
+            i += 1
+            grid[x][y] = random.randint(0, N)
+    if not verification(grid):
+        return generationgrille(N)
+    return grid
+
+def verification(G):
+    for k in range(len(G)):
+        if not verifier_ligne(G, k):
+            return False
+        if not verifier_bloc(G, k):
+            return False
+        if not verify_column(G, k):
+            return False
+    return True
--- sudoku.py
+++ sudoku.py
@@ -1,51 +1,8 @@
-from fonctions import verifier_bloc,verifier_ligne,verify_column,est_complete
+from fonctions import *
 import random
 
-# N = int(input("Taille de grille: "))
 grille = [[1,4,3,2], [3,2,0,4], [4,1,2,3], [2,3,4,1]]
 grille_test_aresoudre = [[0,1,0,0], [0,4,0,0], [0,0,0,0], [1,0,0,4]]
-
-
-def afficher(G):
-    affichage = "------------------"
-    for k in range(len(G)):
-        affichage += f"\n| {G[k][0]} | {G[k][1]} || {G[k][2]} | {G[k][3]} |"
-        if k == 1:
-            affichage += "\n------------------"
-        affichage += "\n" + "-"*(len(G)**2 + 2)
-    return affichage
-
-# print(afficher(grille))
-
-def verification(G):
-    for k in range(len(G)):
-        if not verifier_ligne(G, k):
-            return False
-        if not verifier_bloc(G, k):
-            return False
-        if not verify_column(G, k):
-            return False
-        """ if not est_complete(G): # ce bloc casse la résolution, y aura toujours des 0 avant
-                                   # d'avoir trouvé une solution donc on peut pas le mettre
-            return False"""
-    return True
-# print(verification(grille))
-
-def generationgrille(N):
-    nb = random.randint(abs((N**2)//3 - 1), (N**2)//3) # on ne rempli qu'a peu près 1/3 de la grille au préalable (difficulté)
-    grid = [[0]*N for _ in range(N)] # ambiguité de python on ne peut pas écrire [[0]*N]*N (a cause de la mutabilitée)
-    i = 0
-    while i < nb:
-        x, y = (random.randint(0, N-1), random.randint(0, N-1))
-        #print(x,y, grid[x][y], i)
-        if grid[x][y] == 0:
-            i += 1
-            grid[x][y] = random.randint(0, N)
-    if not verification(grid):
-        return generationgrille(N)
-    return grid
-
-#print(generationgrille(4))
 
 def resolution(G):
     verif = verification(G)
@@ -62,7 +19,3 @@ def resolution(G):
         res = resolution(G)
         if res != None:
             return res
-
-
-# print(resolution(grille))
-#print(resolution(grille_test_aresoudre))
