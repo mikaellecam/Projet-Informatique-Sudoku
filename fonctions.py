11a54bd5babbf87b22fd178e7f54060907318ee3
Mikael Le Cam <mikael.california@gmail.com>
2022-10-06 09:45:03 +0200

    Réorganisation + modification d'affichage

--- fonctions.py
+++ fonctions.py
@@ -3,6 +3,7 @@ import random
 
 def afficher(G):
     if G is None:
+        print("None")
         return
     affichage = "------------------"
     for k in range(len(G)):
@@ -10,7 +11,7 @@ def afficher(G):
         if k == 1:
             affichage += "\n------------------"
         affichage += "\n" + "-"*(len(G)**2 + 2)
-    return affichage
+    print(affichage)
 
 
 def verifier_bloc(G,i):
@@ -84,3 +85,40 @@ def verification(G):
         if not verify_column(G, k):
             return False
     return True
+
+def resolution(G):
+    verif = verification(G)
+    if len(G) == 0 or not verif:
+        return None
+    if est_complete(G):
+        return G
+    for i in range(len(G)):
+        for j in range(len(G)):
+            if G[i][j] == 0:
+                x, y = i, j
+    for k in range(1,len(G)+1):
+        G[x][y] = k
+        res = resolution(G)
+        if res is not None:
+            return res
+
+def gen_grille(N, diff: int):
+    grid = [[0]*N for _ in range(N)] #Grille vide
+    diff_coefs = [0.5,0.35,0.25] #Coefficient de difficulté
+    t = [i for i in range(1,N+1)] #Valeur entre 1 et 4 inclus
+    x,y = random.randint(0,N-1), random.randint(0,N-1) #Position aléatoire de notre valeur aléatoire
+    res = None
+    while res is None and len(t) > 0: #Tant qu'on ne trouve pas de solution, on change le nombre aléatoire et recommence la résolution
+        indexe = random.randint(0,len(t)-1) #Le nombre(aléatoire) qu'on met dans la grille
+        grid[x][y] = t.pop(indexe) #on met le nombre qu'on a définit, et on l'enlève de la liste
+        res = resolution(grid) #on essaie de résoudre
+    if res is not None: #Si on trouve une solution
+        nb = round(N**2*diff_coefs[diff]) #le nombre de chiffre qu'il nous reste dans la grille
+        nbactuel = N**2 #c'est le nombre qu'il y a actuellement dans la grille
+        while nbactuel > nb: 
+            x, y = random.randint(0, N - 1), random.randint(0, N - 1) #positions random dans lesquelles on enlève le chiffre
+            if res[x][y]:
+                res[x][y] = 0 #on remplace le chiffre par un 0
+                nbactuel -= 1 #on a retiré un nombre donc on décrémente de 1 le nombre actuel
+        return res
+    return gen_grille(N, diff) #Si nous n'avons pas trouvé de solution pour la position aléatoire, nous recommencons avec (normalement) une autre position
--- sudoku.py
+++ sudoku.py
@@ -1,53 +1,9 @@
 from fonctions import *
-import random
-
-grille = [[1,4,3,2], [3,2,0,4], [4,1,2,3], [2,3,4,1]]
-grille_test_aresoudre = [[0,1,0,0], [0,4,0,0], [0,0,0,0], [1,0,0,4]]
-
-def gen_grille(N, diff: int):
-    grid = [[0]*N for _ in range(N)]
-    diff_coefs = [0.25,0.35,0.5]
-    t = [i for i in range(1,N+1)]
-    x,y = random.randint(0,N-1), random.randint(0,N-1)
-    res = None
-    while res is None and len(t) > 0:
-        indexe = random.randint(0,len(t)-1)
-        grid[x][y] = t.pop(indexe)
-        res = resolution(grid)
-    if res is not None:
-        nb = round(N**2*diff_coefs[diff])
-        p = N**2
-        while p > nb:
-            x, y = random.randint(0, N - 1), random.randint(0, N - 1)
-            if res[x][y]:
-                res[x][y] = 0
-                p -= 1
-        return res
-    return gen_grille(N, diff)
 
 
-
-
-def resolution(G):
-    verif = verification(G)
-    if len(G) == 0 or not verif:
-        return None
-    if est_complete(G):
-        return G
-    for i in range(len(G)):
-        for j in range(len(G)):
-            if G[i][j] == 0:
-                x, y = i, j
-    for k in range(1,len(G)+1):
-        G[x][y] = k
-        res = resolution(G)
-        if res is not None:
-            return res
-
-
-
-G = gen_grille(4, 1)
-print(afficher(G))
+G = gen_grille(4, 2)
+afficher(G)
+afficher(resolution(G))
 """G = generationgrille(4)
 print(afficher(G))
 print(afficher(resolution(G)))"""
