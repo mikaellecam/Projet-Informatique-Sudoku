59ec43512a37bb050fbaadcde5ad176018ceb0b3
Mikael Le Cam <mikael.california@gmail.com>
2022-10-17 22:47:50 +0200

    Cleaned up main script

--- sudoku.py
+++ sudoku.py
@@ -1,32 +1,4 @@
 from fonctions import *
 
-#t = [[2,3,1,4],[0,4,0,0,],[0,1,4,0],[0,0,0,1]]
-"""t = [[1,0,2,0], [2,4,0,3], [3,0,4,0], [0,0,0,2]]
-afficher(resolution(t))"""
 
-"""
-compteur = 0
-L = []
-for i in range(1000):
-    G = gen_grille(4, 0)
-    x = resolution(G)
-    L.append(G.copy())
-    if x is None:
-        compteur += 1
-    else:
-        L.pop()
-print(compteur)
-"""
-
-"""
-G = gen_grille(4, 0)
-afficher(G)
-afficher(resolution(G))
-"""
-"""G = generationgrille(4)
-print(afficher(G))
-print(afficher(resolution(G)))"""
-
-#jouer()
-fs = gen_grille(9,1)
-afficher(fs)
+jouer()
