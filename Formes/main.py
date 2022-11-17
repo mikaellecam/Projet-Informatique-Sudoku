2a08b5c5f7ec990d910ddea90a948e1295caa61d
Mikael Le Cam <mikael.california@gmail.com>
2022-11-17 09:52:36 +0100

    creation de la liste

--- Formes/main.py
+++ Formes/main.py
@@ -1,6 +1,17 @@
 from math import sqrt
 from PIL import Image
 
+def createfile(x,y):
+    File = ["P3", str(x), str(y), "255"]
+    for i in range(x):
+        for j in range(y):
+            File.append("0 0 0")
+    return File
+
+
+Fichier = createfile(16,16)
+print(Fichier)
+
 def cercle(fichier, coords: tuple, rayon: float, color: tuple):
     """
     fonction permettant de rajouter un cercle de centre coords et d'un certain rayon, avec une certaine couleur
@@ -19,3 +30,9 @@ def cercle(fichier, coords: tuple, rayon: float, color: tuple):
             else:
                 pixels.append((0, 0, 0))
 
+
+
+"""with open('output.ppm', 'w') as f:
+    lines = ["P3",'1024','1024','255','255 0 0', '0 255 0', '0 0 255', '255 255 0', "255 255 255",'0 0 0']
+    for line in lines:
+        f.write(line + "\n")"""
