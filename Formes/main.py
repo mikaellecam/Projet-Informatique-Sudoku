719e1a575a3e5cbc97ebfb78bae26fe28940f1e0
Pektopa <98785454+Pektopa@users.noreply.github.com>
2022-12-01 09:33:04 +0100

    Update main.py

--- Formes/main.py
+++ Formes/main.py
@@ -25,7 +25,17 @@ def main(Fichier ,forme : list,) :
             b = rgb(str(input('rgb')))
             polygone(Fichier,a,b)
 
-
+def rectangle(Fichier ,cord1 , cord2 ,rgb):
+    cord1 = list(cord1) 
+    cord2 = list(cord2)
+    l = int(cord2[0]-cord1[0])
+    L = int(cord2[1]-cord1[1])
+    l= abs(l)
+    L = abs(L)
+    for index1 in range (l):
+        for index2 in range (L):
+            if 0<index1<int(Fichier[1][0]) and 0<index2<int(Fichier[2][0]):
+                Fichier[index2+4][index1] = rgb
 
 
 Fichier = createfile(1024,1024)
